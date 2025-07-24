4300aacfe512383a22b8506c952e0fd8a494d6e8
sadek <sadeksshaikh540@gmail.com>
2025-07-24 22:12:03 +0530

    bgcolor set to curr color even after refresh

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 function App() {
-  const [color, setColor] = useState("skyBlue")
+  const defColor = localStorage.getItem('color') || 'skyBlue'
+  const [color, setColor] = useState(defColor)
 
   return (
     <>
@@ -8,16 +9,29 @@ function App() {
         style={{backgroundColor: color}}>
           <div className="fixed flex fles-wrap justify-center bottom-48 inset-x-0 px-2">
             <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
-              <button id="red" onClick={() => setColor("red")} className="outline-none px-4 py-1 rounded-xl"
-              style={{backgroundColor: "red"}}
-              >Red</button>
-              <button onClick={() => setColor("green")} className="outline-none px-4 py-1 rounded-xl"
-              style={{backgroundColor: "green"}}
-              >Green</button>
-              <button onClick={() => setColor("gray")} className="outline-none px-4 py-1 rounded-xl"
+              <button
+              onClick={() => {
+                setColor("pink")
+                localStorage.setItem('color', 'pink')
+              }} className="outline-none px-4 py-1 rounded-xl"
+              style={{backgroundColor: "pink"}}
+              >Pink</button>
+              <button onClick={() => {
+                setColor("lightgreen")
+                localStorage.setItem('color', 'lightgreen')
+              }} className="outline-none px-4 py-1 rounded-xl"
+              style={{backgroundColor: "lightgreen"}}
+              >Light green</button>
+              <button onClick={() => {
+                setColor("gray")
+                localStorage.setItem('color', 'gray')
+              }}className="outline-none px-4 py-1 rounded-xl"
               style={{backgroundColor: "gray"}}
               >Gray</button>
-              <button onClick={() => setColor("skyBlue")} className="outline-none px-4 py-1 rounded-xl"
+              <button onClick={() => {
+                setColor("skyBlue")
+                localStorage.setItem('color', 'skyBlue')
+              }} className="outline-none px-4 py-1 rounded-xl"
               style={{backgroundColor: "skyBlue"}}
               >skyBlue</button>
             </div>
